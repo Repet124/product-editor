79f56c82cd1ada59b6a3a49f1f9fd114ae7a9f4f
Oleg Bondarenko <hlop4ik124@gmail.com>
2020-08-25 16:45:28 +0300

    props default images and count images

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
 			Radio component
 		</Radio>
 		<Checkbox v-model="arr" :list="preferences.checkbox"></Checkbox> -->
-		<ImageLoader></ImageLoader>
+		<ImageLoader count="3" :defaultImages="files"></ImageLoader>
 	</div>
 </template>
 
@@ -43,7 +43,21 @@ export default {
 			],
 			selectItem: 'second',
 			arr: ['first', 'second'],
-			preferences: preferences
+			preferences: preferences,
+			files: [
+				{
+					url: 'https://r.ddmcdn.com/s_f/o_1/cx_462/cy_245/cw_1349/ch_1349/w_720/APL/uploads/2015/06/caturday-shutterstock_149320799.jpg',
+					file: false
+				},
+				{
+					url: 'https://image.made-in-china.com/202f0j10jzGfdkZWbYqH/Pink-Electric-Fluffy-Cat-Plush-Stuffed-Toy-for-Children.jpg',
+					file: false
+				}
+			],
+			// files : [
+			// 	'https://r.ddmcdn.com/s_f/o_1/cx_462/cy_245/cw_1349/ch_1349/w_720/APL/uploads/2015/06/caturday-shutterstock_149320799.jpg',
+			// 	'https://image.made-in-china.com/202f0j10jzGfdkZWbYqH/Pink-Electric-Fluffy-Cat-Plush-Stuffed-Toy-for-Children.jpg'
+			// ]
 		}
 	}
 }
--- src/components/inputs/imageLoader.vue
+++ src/components/inputs/imageLoader.vue
@@ -1,10 +1,10 @@
 <template>
 	<div>
-		<input type="file" ref="input" multiple hidden @change="show">
+		<input type="file" ref="input" :multiple="multiple > 1" hidden @change="show">
 		<div class="imagePreview_wrapper" v-if="images.length !== 0">
 			<ImageItem v-for="(img, index) in images" :key="index" :src="img.url" @rm="rm(index)"></ImageItem>
 		</div>
-		<button @click="load">Add image</button>
+		<button v-if="images.length < multiple" @click="load">Add image</button>
 	</div>
 </template>
 
@@ -12,14 +12,29 @@
 
 import ImageItem from './image-item.vue';
 
+function parseDefaultImages(images) {
+	if (!images) return [];
+
+	if (!(images instanceof Array)) images = [images];
+
+	if (images.every(item => item.url !== undefined && item.file !== undefined)) return images;
+
+	return images.map(item => ({
+		url: item,
+		file: false
+	}))
+}
+
 export default {
 	name: 'ImageLoader',
 	components: {
 		ImageItem
 	},
+	props: ['count', 'defaultImages'],
 	data: function() {
 		return {
-			images: [],
+			images: parseDefaultImages(this.defaultImages),
+			multiple: this.count ? this.count : 1
 		}
 	},
 	methods: {
