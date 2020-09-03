2cfe7ff12ff4b2d1e9860214d0da9515e4d17085
Oleg Bondarenko <hlop4ik124@gmail.com>
2020-09-03 12:35:28 +0300

    creating prepare methods

--- src/buildProd.js
+++ src/buildProd.js
@@ -1,8 +1,8 @@
-// import preferences from './preferences.js';
+import preferences from './preferences.js';
 
-const methods = {
+const parseMethods = {
 
-	ImageLoader: value => {
+	ImageLoader: (name, value) => {
 		if (value === '') return [];
 		return value.map(item => ({
 			url: item,
@@ -11,9 +11,43 @@ const methods = {
 	}
 }
 
-export function handlerOfComponent(componentName, serverAnswer) {
-	if (methods[componentName]) {
-		return methods[componentName](serverAnswer);
+const prepareMethods = {
+	ImageLoader: value => {
+		return value.map(item => {
+			if (item.file) {
+				return {
+					url: item.url,
+					file: file.name
+				}
+			}
+			return {
+				url: item.url
+			}
+		})
 	}
-	return serverAnswer;
+}
+
+export function prepareProd(type, prodData) {
+	if (methods[componentName]) {
+		return methods[componentName](prodData);
+	}
+	return prodData;
+}
+
+export function parseProd(type, prodData) {
+	let newProd = {},
+		componentName;
+	for(let name in prodData) {
+
+		componentName = preferences.prod[type][name].component;
+
+		if (parseMehods[componentName]) {
+			newProd[name] = parseMehods[componentName](prodData);
+			continue;
+		}
+
+		newProd[name] = prodData[name];
+	}
+
+	return newProd;
 }
