17b21229cb32cdba4f8e0dac523164941124e5fe
Oleg Bondarenko <hlop4ik124@gmail.com>
2020-09-03 13:44:15 +0300

    joinning parse and prepare functions

--- src/buildProd.js
+++ src/buildProd.js
@@ -12,42 +12,60 @@ const parseMethods = {
 }
 
 const prepareMethods = {
-	ImageLoader: value => {
-		return value.map(item => {
+	ImageLoader: (field, value) => {
+		let data = [], files = [], result = {};
+		value.forEach(item => {
 			if (item.file) {
-				return {
+				data.push({
 					url: item.url,
 					file: file.name
-				}
+				});
+				files.push(item.file);
+				return;
 			}
-			return {
+			data.push({
 				url: item.url
-			}
+			});
 		})
+		result[field] = data;
+		result[field+'-files'] = files;
+
+		return result;
 	}
 }
 
 export function prepareProd(type, prodData) {
-	if (methods[componentName]) {
-		return methods[componentName](prodData);
-	}
-	return prodData;
-}
-
-export function parseProd(type, prodData) {
-	let newProd = {},
+	let prod = {},
 		componentName;
 	for(let name in prodData) {
 
 		componentName = preferences.prod[type][name].component;
 
 		if (parseMehods[componentName]) {
-			newProd[name] = parseMehods[componentName](prodData);
+			prod[name] = parseMehods[componentName](prodData);
+			prod = {...prod, ...parseMehods[componentName](name, prodData)};
 			continue;
 		}
 
-		newProd[name] = prodData[name];
+		prod[name] = prodData[name];
+	}
+	return prodData;
+}
+
+export function parseProd(type, prodData, action) {
+	let prod = {},
+		componentName;
+	for(let name in prodData) {
+
+		componentName = preferences.prod[type][name].component;
+
+		if (parseMehods[componentName]) {
+			prod[name] = parseMehods[componentName](prodData);
+			continue;
+		}
+
+		prod[name] = prodData[name];
 	}
 
-	return newProd;
+	return prod;
 }
